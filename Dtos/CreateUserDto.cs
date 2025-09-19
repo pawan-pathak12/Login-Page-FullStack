26fa1360e83faaab07ffc7cb773ea737588dba09
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2025-09-19 11:22:27 +0545

    shift normal validation from js to c#

--- Dtos/CreateUserDto.cs
+++ Dtos/CreateUserDto.cs
@@ -1,8 +1,14 @@
-﻿namespace LoginDemo.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginDemo.Dtos
 {
     public class CreateUserDto
     {
+        [Required(ErrorMessage = "PLease Enter Email Address")]
+        [EmailAddress(ErrorMessage = "Please Enter Email in right format ")]
         public string Email { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Password is Required")]
+        [MinLength(8, ErrorMessage = "Passeord Length Should be greater than 8 words")]
         public string Password { get; set; } = string.Empty; // later: store hashed
     }
 }
